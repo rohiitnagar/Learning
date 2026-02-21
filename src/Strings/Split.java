bc58d11f5ba745526efb6da07abad0b855831aae
Rohit <rohiitnagar@gmail.com>
2026-02-22 02:00:39 +0530

    hi

---
+++ src/Strings/Split.java
@@ -0,0 +1,8 @@
+package Strings;
+
+public class Split {
+    public static void main(String[] args) {
+        String fruits = "apple, banana, mango, grapes";
+        String[] fruitArray = fruits.split(",");
+    }
+}
